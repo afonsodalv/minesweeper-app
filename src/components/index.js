211263801df243987e8d79a20f362dff666e1a6e
afonsodalv <afonso.a.david@gmail.com>
2024-04-30 12:24:49 +0100

    Inicio

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Header } from "./header/header.component";
+export { default as Footer } from "./footer/footer.component";
+export { default as ControlPanel } from "./control-panel/control-panel.component";
