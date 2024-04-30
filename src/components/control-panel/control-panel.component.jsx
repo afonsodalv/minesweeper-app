f4775c87a0576caf8edfd0e412523f2bfc41d5a6
afonsodalv <afonso.a.david@gmail.com>
2024-04-30 14:29:35 +0100

    inicio

---
+++ src/components/control-panel/control-panel.component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function ControlPanel(){
+    return(
+        <section id="panel-control">
+            <form className="form">
+                <h3>Escolha o Nível</h3>
+                <fieldset>
+                    <label>Nível:</label>
+                    <select id="btLevel">
+                        <option seleceted value="0">Selecione...</option>
+                        <option value="1">Básico (9x9)</option>
+                        <option value="2">Intermédio (16x16)</option>
+                        <option value="3">Avançado (30x16)</option>
+                    </select>
+                </fieldset>
+                <button type="button" id="btPlay">Iniciar Jogo</button>
+                <form>
+                    //tentativa de comit
+                </form>
+            </form>
+
+        </section>
+    );
+}
+
+export default ControlPanel;
