46516d96adec1a780a81a0029b759200911fb01f
Pedro Jorge Nunes <Pedro.J.Nunes@criticalsoftware.com>
2024-05-07 00:21:52 +0100

    WIP:  - Added getCellNumber const to change the image based on the Sprite.  - Left some formatting in square.css for current reference, to be deleted.

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
 export { default as HappyFace } from "./images/happy.png";
 export { default as SadFace } from "./images/sad.png";
 export { default as Reset } from "./images/reset.png";
+export { default as Sprite } from "./images/minesweeper-sprite.png";
--- src/components/square/square.component.jsx
+++ src/components/square/square.component.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import "./square.css";
-import {SadFace, Reset} from "../../assets";
+import {Sprite} from '../../assets';
 
-function Square(){
+const getCellNumber = (cellNumber) => {
+    let background;
+    if (typeof cellNumber === 'number') {
+        background = `url(${Sprite}) -${(cellNumber-1) * 17}px 16px`;
+    } else if (cellNumber === 'flag') {
+        // Set background for flag
+    } else if (cellNumber === 'question') {
+        // Set background for question
+    } else if (cellNumber === 'bomb') {
+        // Set background for bomb
+    }
 
+    return {
+        width: '16px',
+        height: '16px',
+        background,
+    };
+};
+
+function Square({ cellNumber }) {
     return (
-        // <div className="square-blank"></div>
-
-        //estava só a experimentar uma cena ;) não ligues a isto
-        <div className='square-blank' data-logo="test">
-            <img 
-                src= {SadFace}
-                className='btInit'
-                alt='square placeholder test'
-            />
-        </div>
+        <div style={getCellNumber(cellNumber)}></div>
     );
 }
 
