e97f3bff7796544fbc5f0439991a2a51d0d5eecc
Pedro Jorge Nunes <Pedro.J.Nunes@criticalsoftware.com>
2024-05-07 00:17:40 +0100

    Refactor of getControlPanelSize and getBoardSize into getGameSettings.

--- src/components/game-panel/game-panel.component.jsx
+++ src/components/game-panel/game-panel.component.jsx
@@ -1,40 +1,38 @@
 import React from "react"
 import {Square} from "../../components";
+import { getGameSettings } from "../../helpers";
 
 
 function GamePanel({numBombs}){
+  // const boardSize = getBoardSize(numBombs);
+  const boardSize = getGameSettings(numBombs);
+  const bombs = generateBombs(boardSize, numBombs);
+  const board = [];
 
-
-    const boardSize = getBoardSize(numBombs);
-    const board = [];
-
-    function getBoardSize(numBombs) {
-
-        
-        switch (numBombs) {
-          case 10:
-            return { rows: 9, cols: 9 };
-          case 40:
-            return { rows: 16, cols: 16 };
-        default:   
-            return { rows: 16, cols: 30 };
-          
-        }
-      }
-
-    for (let i = 0; i < boardSize.cols; i++) {
-        const row = [];
-        for (let j = 0; j < boardSize.rows; j++) {
-          row.push(<Square key={`${i}-${j}`} />);
-        }
-        board.push(<div key={i}>{row}</div>);
-      }
-    
-      return (
-        <div className="game-panel" >
-          {board}
-        </div>
-      );
+  function generateBombs(boardSize, numBombs) {
+    let bombs = new Set();
+    while (bombs.size < numBombs) {
+        let randomRow = Math.floor(Math.random() * boardSize.rows);
+        let randomCol = Math.floor(Math.random() * boardSize.cols);
+        bombs.add(`${randomRow}-${randomCol}`);
     }
+    return bombs;
+  }
+
+  for (let i = 0; i < boardSize.cols; i++) {
+    const row = [];
+    for (let j = 0; j < boardSize.rows; j++) {
+      let isBomb = bombs.has(`${i}-${j}`);
+      row.push(<Square key={`${i}-${j}`} isBomb={isBomb} cellNumber={1} />); // 1 is a placeholder for the cell number
+    }
+    board.push(<div key={i}>{row}</div>);
+  }
+  
+  return (
+    <div className="game-panel" >
+      {board}
+    </div>
+  );
+}
 
 export default GamePanel;
