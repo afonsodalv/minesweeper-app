c9de2a0209e23c05ea965053f3cae719f8aee61e
Pedro Jorge Nunes <Pedro.J.Nunes@criticalsoftware.com>
2024-05-08 23:57:08 +0100

    Feature Expand empty tiles implemented (will probably be refactored). Adjacent square helper function now checks limits.

--- src/components/game-panel/game-panel.component.jsx
+++ src/components/game-panel/game-panel.component.jsx
@@ -1,15 +1,16 @@
-import React, {useState} from "react"
+import React, { useState, useEffect }  from "react"
 import {Square} from "../../components";
-import { getGameSettings } from "../../helpers";
+import { getGameSettings, getAdjacentSquares } from "../../helpers";
 
 
 function GamePanel({numBombs, gameActive}){
-  // const boardSize = getBoardSize(numBombs);
   const boardSize = getGameSettings(numBombs);
-  const bombs = generateBombs(boardSize, numBombs);
-  const board = [];
+  console.log(`Bombs: ${numBombs} - Board Size: ${boardSize.rows}, ${boardSize.cols}`);
 
-
+  const [bombs, setBombs] = useState(new Set());
+  useEffect(() => {
+    setBombs(generateBombs(boardSize, numBombs));
+  }, [numBombs, gameActive]);
 
   function generateBombs(boardSize, numBombs) {
     let bombs = new Set();
@@ -20,21 +21,77 @@ function GamePanel({numBombs, gameActive}){
     }
     return bombs;
   }
+  console.log(`Bombs: ${Array.from(bombs)}`);
+  
 
-  for (let i = 0; i < boardSize.cols; i++) {
-    const row = [];
-    for (let j = 0; j < boardSize.rows; j++) {
-      let isBomb = bombs.has(`${i}-${j}`);
-      row.push(<Square gameActive={gameActive} key={`${i}-${j}`} id={`${i}-${j}`} isBomb={isBomb} bombs={bombs}  />);
-        }
-        board.push(<div key={i}>{row}</div>);
-      }
+  const board = Array(boardSize.rows).fill().map(() => Array(boardSize.cols).fill(
+    <Square 
+      gameActive={gameActive} 
+      key={`default-key`} 
+      id={`default-id`} 
+      isBomb={false} 
+      bombs={new Set()}
+      revealed={false}
+      revealSquare={() => {}}
+    />
+  ));
+  
+  const [revealed, setRevealed] = useState(Array(boardSize.rows).fill().map(() => Array(boardSize.cols).fill(false)));
+  
+  function revealSquare(row, col) {
+
+    const adjacentSquares = getAdjacentSquares(row, col, boardSize.rows, boardSize.cols);
+    console.log(`Adjacent cells: ${adjacentSquares}`);
     
-      return (
-        <div className="game-panel" >
-          {board}
-        </div>
+    setRevealed(prevState => {
+        const newState = [...prevState];
+        adjacentSquares.forEach((square) => {
+          const [adjRow, adjCol] = square.split('-').map(Number);
+          if (revealed[adjRow][adjCol] === false){
+            newState[adjRow][adjCol] = true;
+          }
+        });
+        return newState;
+      });
+      
+    console.log(`Revealed Arr: ${revealed}`);
+  }
+
+
+
+  for (let i = 0; i < boardSize.rows; i++) {
+    for (let j = 0; j < boardSize.cols; j++) {
+      let isBomb = bombs.has(`${i}-${j}`);
+      board[i][j] = (
+        <Square 
+          gameActive={gameActive} 
+          key={`${i}-${j}`} 
+          id={`${i}-${j}`} 
+          isBomb={isBomb} 
+          bombs={bombs}
+          revealed={revealed[i][j]}
+          revealSquare={revealSquare}
+        />
       );
     }
+  }
+
+    
+  return (
+    <div className="game-panel">
+    {/* {board.map((row, i) => (
+      <div key={i}>
+        {row}
+      </div>
+    ))} */}
+    {board[0].map((col, i) => (
+      <div key={i}>
+        {board.map(row => row[i])}
+      </div>
+    ))}
+  </div>
+  
+  );
+}
 
 export default GamePanel;
--- src/components/square/square.component.jsx
+++ src/components/square/square.component.jsx
@@ -1,22 +1,20 @@
 import React, { useState } from "react";
 import "./square.css";
-// import {SadFace, Flag, Reset} from "../../assets";
 import {Sprite} from '../../assets';
-import { countAdjacentBombs, getAdjacentSquares } from "../../helpers";
+import { countAdjacentBombs } from "../../helpers";
 
 
-function Square({gameActive, id, isBomb, bombs}){
+function Square({gameActive, id, isBomb, bombs, revealed, revealSquare}){
 
     const [cellType, setCellType] = useState('-init');
 
     const getCellBackground = (cellType) => {
-        let background, zoom;
+        let background;
         if (typeof cellType === 'number') {
             background = `url(${Sprite}) -${(cellType-1) * 17}px 16px`;
         } 
         else if (cellType === '-init') {
             background = `url(${Sprite}) 0px -51px`;
-            zoom = '1.5';
         } 
         else if (cellType === '-flag') {
             background = `url(${Sprite}) -34px -51px`;
@@ -34,42 +32,52 @@ function Square({gameActive, id, isBomb, bombs}){
         return {
             width: '16px',
             height: '16px',
-            zoom,
+            zoom: '1.5',
             background,
         };
     };
+
 
     const [clicked, setClicked] = useState(false);
     const [x, setX] = useState(3);
 
 
+    if (revealed && !clicked) {
+        handleLeftClick();
+    }
+
     function handleLeftClick() {
         if (!gameActive) return;
-        setClicked(true);
+        
         if(x!==1){
             setX(0);
-        
+            revealed = true;
             const [row, col] = id.split('-').map(Number);
             const nBombs = countAdjacentBombs(row, col, bombs);
+            console.log(`Square component about to call countAdjacentBombs: ${row}, ${col} - ${nBombs} bombs nearby`);
             if(isBomb) {
                 setCellType('-bomb');
             } else if(nBombs === 0) {
                 setCellType('-clicked');
-                // Add your logic for clicking adjacent squares here
-                // clickAdjacentCells(row, col, bombs);
-                
+                if(!clicked) {
+                    console.log(`Square component about to call reveal square: ${row}, ${col}`);
+                    setClicked(true);
+                    revealSquare(row, col);
+                } 
+
             } else {
                 setCellType(nBombs);
             }
-        }   
+            setClicked(true);
+            
+        }
     }
 
     function handleRightClick(event){
         if (!gameActive) return;
         event.preventDefault();
-        setClicked(true);
 
-        if(x && x!==3) {
+        if(!clicked && x!==3) {
             setX(x+1);
             (x+1) === 2 ? setCellType('-question') : setCellType('-init');
         } else if(x===3) {
@@ -79,11 +87,7 @@ function Square({gameActive, id, isBomb, bombs}){
     }
 
     return (
-        <div className='square-blank' style={getCellBackground('-init')} onClick={handleLeftClick} onContextMenu={handleRightClick}>
-            {clicked && x!==3 && (
-            <div style={getCellBackground(cellType)}></div>
-            )}
-        </div>
+        <div className='square-blank' style={getCellBackground(cellType)} onClick={handleLeftClick} onContextMenu={handleRightClick}></div>
     );
 }
 
