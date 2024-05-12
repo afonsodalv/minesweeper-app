744ab2d7bddce17881892e7277e2008ce23818cc
PedroJSNunes <47609381+PedroJSNunes@users.noreply.github.com>
2024-05-12 03:51:33 +0100

    Fix: Closes #14
    This changes updated the button on control panel. Closes #14 and #13 although some code cleanup is still required in the control panel component.

--- src/components/game-panel/game-panel.component.jsx
+++ src/components/game-panel/game-panel.component.jsx
@@ -3,116 +3,122 @@ import {Square} from "../../components";
 import { getGameSettings, getAdjacentSquares } from "../../helpers";
 
 
-function GamePanel({numBombs, gameActive, startTimer, setStartTimer, handleGameEnd, handleGameScore, setGameWon}){
+function GamePanel({numBombs, gameActive, startTimer, setStartTimer, handleGameEnd, handleGameScore, gameWon, setGameWon}){
   
-  const [boardSize, setBoardSize] = useState(getGameSettings(numBombs));
-  const [revealed, setRevealed] = useState(Array(boardSize.rows).fill().map(() => Array(boardSize.cols).fill(false)));
-  const [clickedBomb, setClickedBomb] = useState(false);
-  const [flags, setFlags] = useState(new Set());
+	const [boardSize, setBoardSize] = useState(getGameSettings(numBombs));
+	const [revealed, setRevealed] = useState(Array(boardSize.rows).fill().map(() => Array(boardSize.cols).fill(false)));
+	const [clickedBomb, setClickedBomb] = useState(false);
+	const [flags, setFlags] = useState(new Set());
+	const [bombs, setBombs] = useState(new Set());
 
-  useEffect(() => {
-    setBoardSize(getGameSettings(numBombs));
-  }, [numBombs]);
-  // const boardSize = getGameSettings(numBombs);
-  // console.log(`Bombs: ${numBombs} - Board Size: ${boardSize.rows}, ${boardSize.cols}`);
+	// This effect will update the board size when the number of bombs changes
+	useEffect(() => {
+		setBoardSize(getGameSettings(numBombs));
+	}, [numBombs]);
+	// const boardSize = getGameSettings(numBombs);
+	// console.log(`Bombs: ${numBombs} - Board Size: ${boardSize.rows}, ${boardSize.cols}`);
 
- const [bombs, setBombs] = useState(new Set());
- useEffect(() => {
-    setBombs(generateBombs(boardSize, numBombs));
-  }, [numBombs, gameActive, boardSize]);
+	// This effect will generate the bombs when the game starts
+	useEffect(() => {
+		setBombs(generateBombs(boardSize, numBombs));
+  	}, [numBombs, gameActive, boardSize]);
 
-  function generateBombs(boardSize, numBombs) {
-    let bombs = new Set();
-    while (bombs.size < numBombs) {
-        let randomRow = Math.floor(Math.random() * boardSize.rows);
-        let randomCol = Math.floor(Math.random() * boardSize.cols);
-        bombs.add(`${randomRow}-${randomCol}`);
-    }
-    return bombs;
-  }
-  //console.log(`Bombs: ${Array.from(bombs)}`);
+	function generateBombs(boardSize, numBombs) {
+		let bombs = new Set();
+		while (bombs.size < numBombs) {
+			let randomRow = Math.floor(Math.random() * boardSize.rows);
+			let randomCol = Math.floor(Math.random() * boardSize.cols);
+			bombs.add(`${randomRow}-${randomCol}`);
+		}
+		return bombs;
+	}
+	//console.log(`Bombs: ${Array.from(bombs)}`);
   
-
-  let board = [];
-  for (let i = 0; i < boardSize.rows; i++) {
-    board[i] = [];
-    for (let j = 0; j < boardSize.cols; j++) {
-      let isBomb = bombs.has(`${i}-${j}`);
-      board[i][j] = (
-        <Square 
-        gameActive={gameActive} 
-        key={`${i}-${j}`} 
-        id={`${i}-${j}`} 
-        isBomb={isBomb} 
-        bombs={bombs}
-        revealed={revealed[i][j]}
-        handleGameScore={handleGameScore}
-        revealSquare={revealSquare}
-        setClickedBomb={setClickedBomb}
-        flagSquare={flagSquare}
-        />
-      );
-    }
-  }
+	// This function will create a 2D array of Square components
+	let board = [];
+	for (let i = 0; i < boardSize.rows; i++) {
+		board[i] = [];
+		for (let j = 0; j < boardSize.cols; j++) {
+		let isBomb = bombs.has(`${i}-${j}`);
+		board[i][j] = (
+			<Square 
+			gameActive={gameActive} 
+			gameWon={gameWon}
+			key={`${i}-${j}`} 
+			id={`${i}-${j}`} 
+			isBomb={isBomb} 
+			bombs={bombs}
+			revealed={revealed[i][j]}
+			handleGameScore={handleGameScore}
+			revealSquare={revealSquare}
+			setClickedBomb={setClickedBomb}
+			flagSquare={flagSquare}
+			/>
+		);
+		}
+	}
   
-  
-  function flagSquare(id) {
-    if (flags.has(id)) {
-      setFlags(prevState => {
-        const newState = new Set(prevState);
-        newState.delete(id);
-        return newState;
-      });
-    } else {
-      setFlags(prevState => {
-        const newState = new Set(prevState);
-        newState.add(id);
-        return newState;
-      });
-    }
-  }
+	// This function will flag or unflag a square. It is passed as a prop to the Square component and is called when the right mouse button is clicked.
+	function flagSquare(id) {
+		if (flags.has(id)) {
+		setFlags(prevState => {
+			const newState = new Set(prevState);
+			newState.delete(id);
+			return newState;
+		});
+		} else {
+		setFlags(prevState => {
+			const newState = new Set(prevState);
+			newState.add(id);
+			return newState;
+		});
+		}
+	}
     
-  function revealSquare(row, col, nBombs) {
-    if(!startTimer) setStartTimer(true);
+	// This function will reveal the square and adjacent squares if there are no bombs nearby
+	// it will be recursively called for each adjacent square that has no bombs nearby
+	function revealSquare(row, col, nBombs) {
+		if(!startTimer) setStartTimer(true);
 
-    const adjacentSquares = getAdjacentSquares(row, col, boardSize.rows, boardSize.cols);
-    //console.log(`Adjacent cells: ${adjacentSquares}`);
-    setRevealed(prevState => {
-      const newState = [...prevState];
-      newState[row][col] = true;
-      if(nBombs===0){
-        adjacentSquares.forEach((square) => {
-          const [adjRow, adjCol] = square.split('-').map(Number);
-          if(flags.has(`${adjRow}-${adjCol}`)) return;
-          if (revealed[adjRow][adjCol] === false){
-            newState[adjRow][adjCol] = true;
-          }
-        });
-      }
-      return newState;
-    });
-    //console.log(`Revealed Arr: ${revealed}`);
-  }
+		const adjacentSquares = getAdjacentSquares(row, col, boardSize.rows, boardSize.cols);
+		//console.log(`Adjacent cells: ${adjacentSquares}`);
+		setRevealed(prevState => {
+		const newState = [...prevState];
+		newState[row][col] = true;
+		if(nBombs===0){
+			adjacentSquares.forEach((square) => {
+			const [adjRow, adjCol] = square.split('-').map(Number);
+			if(flags.has(`${adjRow}-${adjCol}`)) return;
+			if (revealed[adjRow][adjCol] === false){
+				newState[adjRow][adjCol] = true;
+			}
+			});
+		}
+		return newState;
+		});
+		//console.log(`Revealed Arr: ${revealed}`);
+	}
 
 
-                  //GAME END LOGIC   >>>>>>
-  
-  useEffect(() => {              
-    if (clickedBomb && gameActive) {
-      setGameWon(0);
-      handleGameEnd(0);
-    }  
-  }, [clickedBomb, gameActive, handleGameEnd]);  
-    
+					//GAME END LOGIC   >>>>>>
+	
+	useEffect(() => {              
+		if (clickedBomb && gameActive) {
+		setGameWon(false);
+		handleGameEnd(false);
+		}  
+	}, [clickedBomb, gameActive, setGameWon, handleGameEnd]);  
+		
 
-  useEffect(() => {
-    const revealedCount = revealed.flat().filter(Boolean).length;
-    if (revealedCount === boardSize.rows * boardSize.cols - numBombs) {
-      handleGameEnd(1);
-    }
-  }, [revealed]);
-  
-                  // <<<<<< GAME END LOGIC
+	useEffect(() => {
+		const revealedCount = revealed.flat().filter(Boolean).length;
+		if (revealedCount === boardSize.rows * boardSize.cols - numBombs) {
+		setGameWon(true);
+		handleGameEnd(true);
+		}
+	}, [revealed, boardSize, numBombs, setGameWon, handleGameEnd]);
+
+					// <<<<<< GAME END LOGIC
 
 
   return (
